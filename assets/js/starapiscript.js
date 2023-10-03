3858381df681e41d92993e1d9265cfaffc5aeb05
khmaister <141692336+khmaister@users.noreply.github.com>
2023-10-03 19:38:27 -0400

    Update starapiscript.js

--- assets/js/starapiscript.js
+++ assets/js/starapiscript.js
@@ -3,7 +3,7 @@ var cityEl = document.getElementById("city");
 //https://api.astronomyapi.com/api/v2/studio/star-chart
 
 
-const authString= btoa('${6edc04ef-2570-4a5e-b297-531d86e90512}:${1c26432e95330d72eb2534bd5b2eac0efe766b1a6dc67fa59a9e83b42bb6349ddaa6faf5194f38332bae31388c9b048f0215fbb238bda25170b9c40061085be353b95d883b9a2b785f5dcc3281372083e3e0571228fda9bf0dcbfc0583e1f688853ae6bda44df5f6dbd6d70a68131ebb}');
+const authString= btoa('${ecdd947d-cf12-4360-bc64-5227f9d20eda}:${1c26432e95330d72eb2534bd5b2eac0efe766b1a6dc67fa59a9e83b42bb6349ddaa6faf5194f38332bae31388c9b048f0215fbb238bda25170b9c40061085be3f391dc14a87a9f6fccf6aee3eb31a78bd63abcecd2cd73cbe27447e69da0a638c0053bac4db140bc2cd5e88b6d7037d9}');
 //star map generator
 document.addEventListener("DOMContentLoaded", function () {
     var client = new AstronomyAPI({
