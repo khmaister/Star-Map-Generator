6226262f928c72d9f661acd614709f885b145400
van khin <khmaister@yahoo.com>
2023-10-05 21:30:11 -0400

    sunrise api functioning

---
+++ assets/js/geocode.js
@@ -0,0 +1,54 @@
+var lat= 36.7264511;
+var lon= 4.4150211;
+var counter = 0
+//Assigning global variables
+var cityName = "London"//document.getElementById("search-form").value;
+var fetchUrl = "https://geocode.maps.co/search?q=" + cityName;
+var submitButton = document.querySelector("#search-form");
+
+//Fetch API 
+function fetchData(){
+    var fetchUrl = "https://geocode.maps.co/search?q=" + cityName;
+
+fetch(fetchUrl).then(
+    function (res){
+    return res.json()
+})
+.then(function (data){
+    console.log(data);
+    var coordinates = data [0]
+    var lat = coordinates.lat;
+    var lon = coordinates.lon;
+    console.log("Coordinates:", coordinates);
+    console.log("Latitude:", lat);
+    console.log("Longitude:", lon);
+})
+}
+
+//Adding event listener to submitButton
+if(submitButton){
+    submitButton.addEventListener("click", async function (event) {
+        event.preventDefault();
+        fetchData();
+        counter = 1;
+        console.log(counter);
+        var data = await sunriseData();
+        console.log(data);
+      });
+    }
+
+
+// Sunrise Api
+//https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400;
+function sunriseData(){
+    if (counter === 1){
+        var sunriseFetch = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon;
+        
+        return fetch(sunriseFetch).then(
+            function (response){
+            //console.log(response.json()); 
+            return response.json();
+            }
+        )
+    }
+}
