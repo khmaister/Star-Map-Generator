21ac37e81dd9a8ab7b61a49b728799f5a05107f1
rickdeakins <rickdeakinsjr@gmail.com>
2023-10-09 21:29:00 -0400

    revisions- no errors in console, placeholder data generating on html, still an issue with the search/date inputs pulling a random city

--- assets/js/geocode.js
+++ assets/js/geocode.js
@@ -9,12 +9,30 @@ var cityName =document.getElementById("search-form").value;
 var fetchUrl = "https://geocode.maps.co/search?q=" + cityName;
 var submitButton = document.querySelector("#search-form");
 
-//Fetch API 
+
+//Adding event listener to submitButton
+if(submitButton){
+    submitButton.addEventListener("click", async function (event) {
+        event.preventDefault();
+        var cityName = document.getElementById("search-form").value;
+
+        var coordinates = fetchData(cityName);
+        if (coordinates) {
+            counter = 1;
+            console.log(counter);
+
+            var data = await sunriseData(coordinates.lat, coordinates.lon);
+            console.log(data);
+        }
+    });
+}
+
+//Fetch Geocoding API 
 function fetchData(){
     var fetchUrl = "https://geocode.maps.co/search?q=" + cityName;
 
-fetch(fetchUrl).then(
-    function (res){
+return fetch(fetchUrl)
+.then(function (res){
     return res.json()
 })
 .then(function (data){
@@ -26,47 +44,35 @@ fetch(fetchUrl).then(
     console.log("Latitude:", lat);
     console.log("Longitude:", lon);
 
-// Call sunriseData function here, after lat and lon are defined
-sunriseData(lat, lon);
+    return { lat, lon };
 })
+.catch(function (error) {
+    console.error("Error fetching data:", error);
+    return null;
+});
 }
 
-//Adding event listener to submitButton
-if(submitButton){
-
-    submitButton.addEventListener("click", async function (event) {
-
-        event.preventDefault();
-        fetchData();
-        counter = 1;
-        console.log(counter);
-
-        var data = await sunriseData(lat,lon);
-        console.log(data);
-
-      });
-    }
-
-
-// Sunrise Api
+// Fetch Sunrise Api
 //https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400;
 function sunriseData(lat, lon){
     if (counter === 1){
-
         var sunriseFetch = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon;
         
-        return fetch(sunriseFetch).then(
-            function (response){
-            console.log(response.json()); 
+        return fetch(sunriseFetch)
+        .then(function (response){
+            //console.log(response.json()); 
             return response.json();
             }
         )
         //pull data from JSON
         .then(function (results){
-        var promise = results [0]
-        var sunriseTime = results.sunrise;
-        var sunsetTime = results.sunset;
-        console.log("Sunrise Data:", promise);
+        //var promise = results [0]
+        //var sunriseTime = results.sunrise;
+        //var sunsetTime = results.sunset;
+        var sunriseTime = results.results.sunrise;
+        var sunsetTime = results.results.sunset;
+            
+        //console.log("Sunrise Data:", promise);
         console.log("Sunrise Time:", sunriseTime);
         console.log("Sunset Time:", sunsetTime); 
 
@@ -85,6 +91,9 @@ function sunriseData(lat, lon){
         targetElement.appendChild(sunriseHTML);
         targetElement.appendChild(sunsetHTML);
     })
+    .catch(function (error) {
+        console.error("Error fetching sunrise data:", error);
+    });
     }
 
 }
