9fce9ee646f257fb31cfaa7939c34749c2848d41
KC-Nick <139719585+KC-Nick@users.noreply.github.com>
2023-10-10 18:53:48 -0400

    Revert "modify JS file"

--- assets/js/geocode.js
+++ assets/js/geocode.js
@@ -61,7 +61,6 @@ if (submitButton) {
     } else {
       alert("Please log in first to save the city.");
     }
-    cityNameInput.value = "";
   });
 }
 
@@ -78,7 +77,6 @@ if (loginButton) {
       cityNameInput.value = savedCity;
     } else {
       saveUsernameToLocalStorage(username);
-      cityNameInput.value = "";
     }
   });
 }
@@ -157,18 +155,14 @@ function sunriseData(lat, lon) {
         .then(function (results) {
           var sunriseTime = results.results.sunrise;
           var sunsetTime = results.results.sunset;
-          var cityName = cityNameInput.value;
 
           // Display the data in the UI
           var targetElement = document.getElementById("sunriseset");
           targetElement.innerHTML = "";
-          var cityHTML = document.createElement("h1");
-          cityHTML.textContent = "City of " + cityName;
           var sunriseHTML = document.createElement("p");
           sunriseHTML.textContent = "Sunrise Time: " + sunriseTime;
           var sunsetHTML = document.createElement("p");
           sunsetHTML.textContent = "Sunset Time: " + sunsetTime;
-          targetElement.appendChild(cityHTML);
           targetElement.appendChild(sunriseHTML);
           targetElement.appendChild(sunsetHTML);
         })
